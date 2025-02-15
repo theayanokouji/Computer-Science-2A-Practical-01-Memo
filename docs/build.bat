545d9672c1837911239e070a399ae44d3f202826
Amos Thando Mpofu <theliteprogrammer@gmail.com>
2025-02-15 10:23:11 +0200

    Updated files

--- docs/build.bat
+++ docs/build.bat
@@ -4,13 +4,14 @@ set BIN=bin
 set DATA=data
 set DOCS=docs
 set LIB=lib
+set ACSSE=acsse\csc2a
 set JAVA_HOME=C:\jdk-21\bin
 
 REM update the PATH environment variables
 set PATH=%PATH%;%JAVA_HOME%
 
 REM compile and add my java byte code to the bin folder
-javac ..\%SRC%\Main.java > ..\%BIN%
+javac ..\%SRC%\Main.java ..\%SRC%\%ACSSE%\*.java -d ..\%BIN%
 
 REM run the java byte code
 java -cp ..\%BIN%\ Main
